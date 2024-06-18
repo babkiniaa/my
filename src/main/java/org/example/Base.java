ec4faede7ff4dafb99c80f86493cc33f5d17c7b7
babkiniaa <jaroslaval@rambler.ru>
2024-06-18 17:08:06 +0300

    точно Все

--- src/main/java/org/example/Base.java
+++ src/main/java/org/example/Base.java
@@ -1,28 +1,50 @@
 package org.example;
 
+import lombok.NonNull;
+
+import java.lang.Exception;
+
 @Inheritance(day = 1)
 public class Base {
 }
 
-class Child extends Base{
+class Child extends Base {
     @RepeatableAnnotation(hour = 1, forever = 12, description = "hi")
-    public void process(DataContainer dataContainer){
-
+    public void process(DataContainer dataContainer) {
+        try {
+            dataContainer.setDay(12);
+            dataContainer.setTemp(22);
+            dataContainer.setDescription("Холодно, сидим дома");
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
     }
 }
 
-class Child2 extends Base{
+class Child2 extends Base {
     @RepeatableAnnotation(hour = 2, forever = 13, description = "bye")
-    public void process(DataContainer dataContainer){
-
+    public void process(@NonNull DataContainer dataContainer) {
+        try {
+            dataContainer.setDay(1);
+            dataContainer.setTemp(2);
+            dataContainer.setDescription("Холодно, иди гуляй");
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
     }
 }
 
 @Inheritance(day = 1)
-class Child3 extends Base{
+class Child3 extends Base {
     @RepeatableAnnotation(hour = 12, forever = 1, description = "Who i")
-    public void process(DataContainer dataContainer){
-
+    public void process(DataContainer dataContainer) {
+        try {
+            dataContainer.setDay(11);
+            dataContainer.setTemp(21);
+            dataContainer.setDescription("Я сам не знаю");
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
     }
 }
 
---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class test {
+    public static void main(String[] args){
+        Child child = new Child();
+        child.process(null);
+    }
+
+
+}
